72dee7dd1626ea8ae938fc805248590cb8bea654
weiying <weiyingwang@g.harvard.edu>
2021-07-22 13:07:07 -0400

    Fix interpolation bug and indexing bug

--- src/WSR_Util.cpp
+++ src/WSR_Util.cpp
@@ -487,8 +487,8 @@ std::pair<nc::NdArray<std::complex<double>>,nc::NdArray<double>> WSR_Util::getFo
                 if(interpolate_phase) 
                 {
                     auto central_snum = nc::NdArray<double>(1, 1) = 15.5;
-                    auto xp = nc::arange<double>(0, 31);
-                    auto fp = unwrap(nc::angle(temp1(0,temp1.cSlice())));
+                    auto xp = nc::arange<double>(0, 30);
+                    auto fp = unwrap(nc::angle(temp1(0,nc::Slice(0,30))));
                     auto mag1 = nc::abs(temp1(0,15));
                     auto mag2 = nc::abs(temp1(0,16));
                     auto fitted =  nc::polynomial::Poly1d<double>::fit(xp.transpose(),fp.transpose(),1);
