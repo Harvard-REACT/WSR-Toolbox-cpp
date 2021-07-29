4e0875e110090cb585f3891dea6452bb3d2d43de
Ninad Jadhav <njadhav@g.harvard.edu>
2021-07-29 14:57:24 -0400

    updated

--- viz_latest_output.sh
+++ viz_latest_output.sh
@@ -3,8 +3,10 @@
 #Fetch the files from the RX_SAR robot (enable password less ssh)
 rsync --ignore-existing up-board-01@192.168.1.15:~/REACT-Projects/WSR-Toolbox-cpp/data/online_data/* $1
 
+#rsync --ignore-existing up-board-03@192.168.1.30:~/REACT-Projects/WSR-Toolbox-cpp/data/online_data/* $1
+
 echo "Running the toolbox code on latest data files"
-../wsr_build/remote_processing $1 $2
+../wsr_build/remote_processing $1 $2 1
 
 num_robot=0
 icr=1
@@ -27,14 +29,14 @@ do
     echo "CSI Phase for $csi_phase_fn"
     python3.8 viz_channel_data.py --file ../debug/$csi_phase_fn
 
-    #traj_pkt_fn=$(ls -t1 ../debug |  head -n $nrf | grep $tx | grep packet)
-    #echo "Packet distribution along the trajectory for $traj_pkt_fn"
-    #python3.8 viz_traj.py --file ../debug/$traj_pkt_fn
+    traj_pkt_fn=$(ls -t1 ../debug |  head -n $nrf | grep $tx | grep packet)
+    echo "Packet distribution along the trajectory for $traj_pkt_fn"
+    python3.8 viz_traj.py --file ../debug/$traj_pkt_fn
 
     profile_fn=$(ls -t1 ../debug |  head -n $nrf | grep $tx | grep aoa)
     echo "AOA profile for $profile_fn"
     #python3.8 visualize_aoa_profile.py --file ../debug/$profile_fn
-    #python3.8 viz_aoa_matlab.py --file ../debug/$profile_fn
+    python3.8 viz_aoa_matlab.py --file ../debug/$profile_fn
     
 
 done
