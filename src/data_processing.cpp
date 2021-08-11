5c94d6bd7441357cb8db75c31352f1aa5c2cb2f9
Ninad Jadhav <njadhav@g.harvard.edu>
2021-08-11 17:40:27 -0400

    read id from json file

--- src/data_processing.cpp
+++ src/data_processing.cpp
@@ -20,11 +20,10 @@ int main(int argc, char *argv[])
 
     string foldername = argv[1];
     string traj_type = argv[2];
-    int loc_idx = std::stoi(argv[4]);
     int start_integer = std::stoi(argv[3]);
     string rx_csi_pre = "csi_rx_";
     string tx_csi_pre = "csi_";
-    string traj_pre, true_traj_pre; 
+    string traj_pre, true_traj_pre, loc_idx; 
     
     true_traj_pre = "rx_trajectory_";
     if (traj_type == "odom")
@@ -157,6 +156,7 @@ int main(int argc, char *argv[])
 
       //Get all True AOA angles
       nlohmann::json true_positions_tx = TX_gt_positions["true_tx_positions"];
+      loc_idx = TX_gt_positions["true_tx_positions"]["value"].dump();
       auto all_true_AOA = utils.get_true_aoa_v2(true_mean_pos, true_positions_tx);
 
       run_module.calculate_AOA_profile(rx_robot_csi,tx_robot_csi,displacement,trajectory_timestamp);
