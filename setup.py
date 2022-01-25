b2a9702bab50f768997179468e4f12da7d77c792
Ninad Jadhav <nhjadhav@asu.edu>
2022-01-25 18:54:43 -0500

    openmp for cython

--- setup.py
+++ setup.py
@@ -15,7 +15,11 @@ extra_compile_args = sysconfig.get_config_var('CFLAGS').split()
 setup(ext_modules=cythonize(Extension(
                 'scripts.libs.wsr_module',
                 sources=['Cython_modules/wsr_module.pyx'],
-                include_dirs=['include','include/csitoolbox','src'],
+                include_dirs=['include',
+                              'include/csitoolbox',
+                              'src',
+                              os.path.expanduser('~')+'/Downloads/boost_1_68_0'],
                 language="c++",
-		extra_compile_args=['-std=c++14']
+		            extra_compile_args=['-std=c++14', '-O3', '-fopenmp'],
+                extra_link_args=['-lgomp', '-lpthread', '-lm', '-ldl']
     )))
