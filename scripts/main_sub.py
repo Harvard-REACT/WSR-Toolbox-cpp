7176458683934c7db58355fd6076948fea75b2df
test-u18 <njadhav@g.harvard.edu>
2022-02-01 15:40:23 -0500

    updated

--- scripts/main_sub.py
+++ scripts/main_sub.py
@@ -8,6 +8,7 @@ Author: Ninad Jadhav, Weiying Wang
 import rospy
 from wsr_toolbox_cpp.msg import wsr_aoa_array
 import numpy as np
+from os.path import expanduser
 
 def wsr_cb(msg):
     print("######################### Got message ######################")
@@ -18,8 +19,9 @@ def wsr_cb(msg):
         print("Profile variance: "+ str(tx.profile_variance))
         print("Profile saved as profile_"+tx.id+".csv")
 
+        homedir = expanduser("~")
         aoa_profile = np.asarray(tx.aoa_profile).reshape((tx.azimuth_dim, tx.elevation_dim))
-        np.savetxt('profile_'+tx.id+'.csv', aoa_profile, delimiter=',')
+        np.savetxt(homedir+'/catkin_ws/src/WSR-Toolbox-cpp/debug/profile_'+tx.id+'.csv', aoa_profile, delimiter=',')
 
 def main():
     
